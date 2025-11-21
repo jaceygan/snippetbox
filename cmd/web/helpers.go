05e230ee8c47e66b1a2e54c133c14553a86d7ce8
jaceygan <jaceygan@gmail.com>
2025-11-21 08:14:45 +0800

    completed template caching

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
 	"runtime/debug"
 )
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
+	// Retrieve the appropriate template set from the cache based on the page
+	// name (like 'home.tmpl'). If no entry exists in the cache with the
+	// provided name, then create a new error and call the serverError() helper
+	// method that we made earlier and return.
+	ts, ok := app.templateCache[page]
+	if !ok {
+		err := fmt.Errorf("the template %s does not exist", page)
+		app.serverError(w, r, err)
+		return
+	}
+
+	w.WriteHeader(status)
+
+	// Execute the template set and write the response body. Again, if there
+	// is any error we call the serverError() helper.
+	err := ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+}
 
 func (app *application) serverInfo(r *http.Request, message string) {
 	var (
