6e9e9574ee32df0d3c7ad1e3553d864c447e3952
jaceygan <jaceygan@gmail.com>
2025-12-12 16:31:27 +0800

    completed chapter 10.6

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -14,8 +14,9 @@ import (
 
 func (app *application) newTemplateData(r *http.Request) templateData {
 	return templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		isAuthenticated: app.isAuthenticated(r),
 	}
 }
 
@@ -82,4 +83,8 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 	}
 
 	return nil
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 }
