0f1f6f6b5df6bd2e37e0d6dace074aed95d9155c
jaceygan <jaceygan@gmail.com>
2025-12-12 17:21:06 +0800

    completed chapter 10

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/go-playground/form/v4"
+	"github.com/justinas/nosurf"
 )
 
 func (app *application) newTemplateData(r *http.Request) templateData {
@@ -17,6 +18,7 @@ func (app *application) newTemplateData(r *http.Request) templateData {
 		CurrentYear:     time.Now().Year(),
 		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
 		isAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 
